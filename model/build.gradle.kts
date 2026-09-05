2c3329e23ae976801a9019c29609c14cbd788b5c
rutvik24 <rutviknabhoya2001@gmail.com>
2026-09-05 23:03:08 +0530

    Add GitHub Actions workflow for publishing Compose Navigation artifacts to Maven Central; update version to 0.1.0 in build.gradle.kts files

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.taqlyn.nav"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
--- navigation3/build.gradle.kts
+++ navigation3/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.taqlyn.nav"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 
 android {
     namespace = "com.taqlyn.nav.compose.navigation3"
